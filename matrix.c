f1d8c3d818c93161b824003047bbeadc222367d5
Chirag <lcs2019069@iiitl.ac.in>
2020-01-25 19:31:41 +0530

    fn modified

--- matrix.c
+++ matrix.c
@@ -1,55 +1,36 @@
 #include<stdio.h>
 int main()
 {
-
  int A[3][3],B[3][3],C[3][3],i,j;
  printf("Enter any nine numbers for matrix A");
  for(i=0;i<=2;i++)
     for(j=0;j<=2 ;j++)
-
+{
  scanf("%d",&A[i][j]);
+}
 
  printf("Enter any nine numbers for matrix B");
  for(i=0;i<=2;i++)
-for(j=0;j<=2;j++)
-            scanf("%d",&B[i][j]);
+ for(j=0;j<=2;j++)
+{
+ scanf("%d",&B[i][j]);
+}
 
-
-   for(i=0;i<=2;i++)
+ printf("multiplication of two  matrix:");
+ for(i=0;i<=2;i++)
+ {
+  for(j=0;j<=2;j++)
+  {
+   for(k=0;k<=2;k++)
    {
-       for(j=0;j<=2;j++){
 
+     C[i][j]=A[i][j]*B[i][j];
+     printf("%d ",C[i][j]);
+   }
 
-    C[i][j]=A[i][j]*B[i][j];
-    printf("%d ",C[i][j]);
+  }
 
-       }
-
-  printf("\n");  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  printf("\n"); 
+ }
 
 }
