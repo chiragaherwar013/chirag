5e7105d78410584d48627985e5cca2732777ed89
Chirag <lcs2019069@iiitl.ac.in>
2020-01-25 18:54:03 +0530

    add fn modified

---
+++ recursive2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+
+    int a,b,c,d,e,sum,num,abcde;
+    printf("Enter numbers for the five digit integer");
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+   num=abcde;
+    printf("the number is %d",&num);
+    scanf("%d",&num);
+    sum=a+b+c+d+e;
+    printf("the sum of entered number is %d",sum);
+
+
+
+
+}
